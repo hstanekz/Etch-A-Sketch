94ed33a5a589332f9008b077838828fac15a0abc
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-03-27 15:28:45 -0700

    Pixel size manipulator and multi color feature

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Etch-A-Sketch
+# Etch-A-Sketch:
+
+Initially started with a 16x16 grid implemented using a JavaScript for-loop and flex-box.
+Went on to implement a hover feature that colors each grid once event is triggered.
+Finally added a simple interface to manipulate pixel size and various sketch colors.
--- script.js
+++ script.js
@@ -2,7 +2,16 @@ let gridSize = 16;
 let gridColor = 'black';
 
 const container = document.querySelector('#container');
-const button = document.querySelector('.new-grid');
+const slider = document.querySelector('.new-grid');
+const red = document.querySelector('.red')
+const orange = document.querySelector('.orange')
+const yellow = document.querySelector('.yellow')
+const green = document.querySelector('.green')
+const blue = document.querySelector('.blue')
+const indigo = document.querySelector('.indigo')
+const violet = document.querySelector('.violet')
+const white = document.querySelector('.white')
+const black = document.querySelector('.black')
 
 
 function createGrid(gridSize) {
@@ -21,8 +30,29 @@ div.addEventListener('mouseover', () => {
 });
 }
 
-button.addEventListener('click', () => {
-    let newSize = parseInt(prompt("Enter a new size: (Max 100)"));
+function getColor() {
+    red.addEventListener('click', ()=> {
+        gridColor = 'red';})
+    orange.addEventListener('click', ()=> {
+        gridColor = 'orange';})
+    yellow.addEventListener('click', ()=> {
+        gridColor = '#ffff53';})
+    green.addEventListener('click', ()=> {
+        gridColor = 'lime';})
+    blue.addEventListener('click', ()=> {
+        gridColor = '#1c05ff';})
+    indigo.addEventListener('click', ()=> {
+        gridColor = '#6e00bf';})
+    violet.addEventListener('click', ()=> {
+        gridColor = 'violet';})
+    white.addEventListener('click', ()=> {
+        gridColor = 'white';})
+    black.addEventListener('click', ()=> {
+            gridColor = 'black';})
+}
+
+slider.addEventListener('input', () => {
+    let newSize = parseInt(slider.value);
     
     //input validation
     if (isNaN(newSize) || (newSize < 1) || (newSize > 100)) {
@@ -34,4 +64,6 @@ button.addEventListener('click', () => {
 
 })
 
+getColor();
 createGrid(gridSize);
+
