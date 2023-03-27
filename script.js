7dc9ac50510a3ffc5b1260c977e240dd74c7160e
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-03-27 13:44:40 -0700

    Various grid layout generation added

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-let container = document.querySelector('#container');
-let button = document.querySelector('.new-grid');
+let gridSize = 16;
+let gridColor = 'black';
 
-for (let i = 0; i < 256; ++i) {
-    let grid = document.createElement('div');
-    grid.classList.add('grid');
-    container.appendChild(grid);
-}
+const container = document.querySelector('#container');
+const button = document.querySelector('.new-grid');
+
+
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize * gridSize; ++i) {
+        let grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.style.width = `calc(100% / ${gridSize})`;
+        grid.style.height = `calc(100% / ${gridSize})`;
+        container.appendChild(grid);
+    }
+
 let divs = document.querySelectorAll('.grid');
-
-function changeColor(div) {
-    div.style.backgroundColor = "black";
-}
-
-divs.forEach((div) =>
+divs.forEach((div) => {
 div.addEventListener('mouseover', () => {
-changeColor(div)}));
+    div.style.backgroundColor = gridColor;});
+});
+}
 
 button.addEventListener('click', () => {
-    prompt("Enter a new size: (Max 100)")
+    let newSize = parseInt(prompt("Enter a new size: (Max 100)"));
+    
+    //input validation
+    if (isNaN(newSize) || (newSize < 1) || (newSize > 100)) {
+        alert("Invalid Size: Please enter a number between 1 and 100.")
+        return;
+    }
+    container.innerHTML = ""; //clear grid
+    createGrid(newSize);
+
 })
 
+createGrid(gridSize);
